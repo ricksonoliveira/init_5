45e7ec6c810ec56944f5df61cf66ba0355a4ad51
Alisson <wildenalisson@outlook.com>
2017-11-04 22:38:51 -0200

    espaço

--- exemplos-em-python3/descompactar.py
+++ exemplos-em-python3/descompactar.py
@@ -14,33 +14,33 @@ ext = str(arquivo[-4:])    # De 4 caracteres
 # tar.bz2
 if ext_8 == '.tar.bz2':
     print('\ntar -jxvf {}\n\n...'.format(arquivo))
-    system('tar -jxvf {}'.format(arquivo))
+    system('tar -jxvf "{}"'.format(arquivo))
 
 # tar.gz
 elif ext_7 == '.tar.gz':
     print('\ntar -vzxf {}\n\n...'.format(arquivo))
-    system('tar -vzxf {}'.format(arquivo))
+    system('tar -vzxf "{}"'.format(arquivo))
 
 # zip
 elif ext == '.zip':
     print('\nunzip {}\n\n...'.format(arquivo))
-    system('unzip {}'.format(arquivo))
+    system('unzip "{}"'.format(arquivo))
 
 # rar
 elif ext == '.rar':
     print('\nunrar x {}\n\n...'.format(arquivo))
-    system('unrar x {}'.format(arquivo))
+    system('unrar x "{}"'.format(arquivo))
 
 
 # tar
 elif ext == '.tar':
     print('\ntar -xvf {}\n\n...'.format(arquivo))
-    system('tar -xvf {}'.format(arquivo))
+    system('tar -xvf "{}"'.format(arquivo))
 
 # bz2
 elif ext == '.bz2':
     print('\nbunzip2 -d {}\n\n...'.format(arquivo))
-    system('bunzip2 -d {}'.format(arquivo))
+    system('bunzip2 -d "{}"'.format(arquivo))
 
 else:
     print('Desculpe, extenção não suportada :(')
